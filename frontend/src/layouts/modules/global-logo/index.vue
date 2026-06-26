4d2c0cacae8b5c4067d024745e7c467086b1dceb
Yeung <498832514@qq.com>
2026-06-26 14:36:41 +0800

    style：登录界面样式，以及menu样式补充

--- frontend/src/layouts/modules/global-logo/index.vue
+++ frontend/src/layouts/modules/global-logo/index.vue
@@ -28,7 +28,7 @@ const logoClass = computed(() => ({
 
 <template>
   <RouterLink to="/" :class="logoClass">
-    <img src="@/assets/imgs/logo.png" alt="" class="w-28px h-28px object-contain mr-2px" />
+    <img src="@/assets/imgs/logo2.png" alt="" class="w-24px h-24px object-contain mr-4px" />
     <h2 v-show="showTitle" class="pl-8px text-16px font-bold transition duration-300 ease-in-out"
       :class="themeStore.isLightSider ? 'text-#fff' : 'text-#pimary'">
       {{ $t('system.title') }}
