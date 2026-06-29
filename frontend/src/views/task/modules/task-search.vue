ff1a29be3d854453c8fad28ca1f521caafbf33f5
Yeung <498832514@qq.com>
2026-06-29 17:32:07 +0800

    style：任务管理筛选区域阴影问题

--- frontend/src/views/task/modules/task-search.vue
+++ frontend/src/views/task/modules/task-search.vue
@@ -76,48 +76,50 @@ onMounted(() => {
 </script>
 
 <template>
-  <NCard :bordered="false" size="small" class="card-wrapper">
-    <NForm :model="model" label-placement="left" :label-width="80">
-      <NGrid responsive="screen" item-responsive>
-        <NFormItemGi span="24 s:12 m:6" label="任务名称" path="name" class="pr-24px">
-          <NInput v-model:value="model.name" placeholder="请输入任务名称" clearable />
-        </NFormItemGi>
-        <NFormItemGi span="24 s:12 m:6" label="任务类型" path="task_type" class="pr-24px">
-          <NSelect v-model:value="model.task_type" :options="taskTypeOptions" placeholder="请选择类型" clearable />
-        </NFormItemGi>
-        <NFormItemGi span="24 s:12 m:6" label="启用状态" path="enabled" class="pr-24px">
-          <NSelect v-model:value="model.enabled" :options="enabledOptions" placeholder="请选择状态" clearable />
-        </NFormItemGi>
-        <NFormItemGi span="24 s:12 m:6" label="场景地图" path="map_id" class="pr-24px">
-          <NSelect
-            v-model:value="model.map_id"
-            :options="mapOptions"
-            placeholder="请选择场景地图"
-            filterable
-            clearable
-            @update:value="handleMapChange"
-          />
-        </NFormItemGi>
-        <NFormItemGi span="24 s:12 m:6" label="机器人" path="robot_id" class="pr-24px">
-          <NSelect v-model:value="model.robot_id" :options="filteredRobotOptions" placeholder="请选择机器人" filterable clearable />
-        </NFormItemGi>
-      </NGrid>
-      <NSpace class="mt-16px w-full" justify="end">
-        <NButton @click="resetModel">
-          <template #icon>
-            <icon-ic-round-refresh class="text-icon" />
-          </template>
-          {{ $t('common.reset') }}
-        </NButton>
-        <NButton type="primary" ghost @click="search">
-          <template #icon>
-            <icon-ic-round-search class="text-icon" />
-          </template>
-          {{ $t('common.search') }}
-        </NButton>
-      </NSpace>
-    </NForm>
-  </NCard>
+  <div class="px-12px py-6px">
+    <NCard :bordered="false" size="small" class="card-wrapper">
+      <NForm :model="model" label-placement="left" :label-width="80">
+        <NGrid responsive="screen" item-responsive>
+          <NFormItemGi span="24 s:12 m:6" label="任务名称" path="name" class="pr-24px">
+            <NInput v-model:value="model.name" placeholder="请输入任务名称" clearable />
+          </NFormItemGi>
+          <NFormItemGi span="24 s:12 m:6" label="任务类型" path="task_type" class="pr-24px">
+            <NSelect v-model:value="model.task_type" :options="taskTypeOptions" placeholder="请选择类型" clearable />
+          </NFormItemGi>
+          <NFormItemGi span="24 s:12 m:6" label="启用状态" path="enabled" class="pr-24px">
+            <NSelect v-model:value="model.enabled" :options="enabledOptions" placeholder="请选择状态" clearable />
+          </NFormItemGi>
+          <NFormItemGi span="24 s:12 m:6" label="场景地图" path="map_id" class="pr-24px">
+            <NSelect
+              v-model:value="model.map_id"
+              :options="mapOptions"
+              placeholder="请选择场景地图"
+              filterable
+              clearable
+              @update:value="handleMapChange"
+            />
+          </NFormItemGi>
+          <NFormItemGi span="24 s:12 m:6" label="机器人" path="robot_id" class="pr-24px">
+            <NSelect v-model:value="model.robot_id" :options="filteredRobotOptions" placeholder="请选择机器人" filterable clearable />
+          </NFormItemGi>
+        </NGrid>
+        <NSpace class="mt-16px w-full" justify="end">
+          <NButton @click="resetModel">
+            <template #icon>
+              <icon-ic-round-refresh class="text-icon" />
+            </template>
+            {{ $t('common.reset') }}
+          </NButton>
+          <NButton type="primary" ghost @click="search">
+            <template #icon>
+              <icon-ic-round-search class="text-icon" />
+            </template>
+            {{ $t('common.search') }}
+          </NButton>
+        </NSpace>
+      </NForm>
+    </NCard>
+  </div>
 </template>
 
 <style scoped></style>
